e266dd271e3ac49934444c91ddea53cf8c95e59a
marcinczerny <czernymarcin@gmail.com>
2019-06-26 06:18:46 +0000

    Changes to be committed: 	modified:   CMakeLists.txt 	new file:   msg/Control.msg 	modified:   package.xml 	new file:   package.xml.save 	modified:   scripts/ControlArdu.py

--- scripts/ControlArdu.py
+++ scripts/ControlArdu.py
@@ -2,12 +2,61 @@
 import rospy
 import serial
 import constants
+
 #import pygame
 import os
 import time
-from std_msgs.msg import String, Int16, Float32 
+from std_msgs.msg import String, Int16, Float32, Int16MultiArray
+from ppsk.msg import Control
+class order:
+    def __init__(self):
+        self.order = 0
+        self.OrderProperties=0
+        self.SerialMessage = b""
+        self.Change = False
+    
+    def ChangeOrder(self,newOrder):
+        self.order = newOrder
+        self.Change = True
 
+    def ChangeOrderProperties(self,newProperties):
+        self.OrderProperties = newProperties
+        self.Change = True
+    
+    def ChangeOrderAndProperties(self,newOrder,newProperties):
+        self.OrderProperties = newProperties
+        self.order = newOrder
+        self.Change = True
 
+    def ReadOrder(self):
+        self.Change = False
+        return (self.order,self.OrderProperties)
+
+    def CheckChange(self):
+        return self.Change
+
+    def ReadSerialMessage(self):
+        return self.SerialMessage
+
+    def GenerateSerialMessage(self):
+        self.Change = False
+        if self.order == constants.CONST_SERIAL_RPI_START:
+            self.SerialMessage = bytearray([constants.CONST_SERIAL_RPI_START]) 
+            return True
+        elif self.order == constants.CONST_SERIAL_RPI_DIRECTION:
+            self.SerialMessage = bytearray([constants.CONST_SERIAL_RPI_DIRECTION, self.OrderProperties])
+            return True
+        elif self.order == constants.CONST_SERIAL_RPI_SPEED:
+            self.SerialMessage = bytearray([constants.CONST_SERIAL_RPI_SPEED, self.OrderProperties])
+            return True
+        elif self.order == constants.CONST_SERIAL_RPI_INITIALIZED:
+            self.SerialMessage = bytearray([constants.CONST_SERIAL_RPI_START])
+            return True
+        elif self.order == constants.CONST_SERIAL_RPI_STOP:
+            self.SerialMessage = bytearray([constants.CONST_SERIAL_RPI_STOP])
+            return True
+        else:
+            return False     
 class state:
     def __init__(self):
         self.limitSwitches = 15
@@ -35,18 +84,25 @@ class state:
     def GetLimitSwitches(self):
         return self.limitSwitches
     
-
 ser = None
-def callback(data):
-    global ser
-    rospy.loginfo(rospy.get_caller_id() + data.data)
-
+orderObject = None
 def receiveOrders(data):
     global ser
+    global orderObject
+
+    orderObject.ChangeOrderAndProperties(data.Order,data.OrderProperty)
+    rospy.loginfo(data.Order)
+    rospy.loginfo(data.OrderProperty)
+    # orderObject.ChangeOrder(data.Order)
+    # orderObject.ChangeOrderProperties(data.OrderProperty)
+
     #if data.data =  constants.   
 
 def talker():
     global ser
+    global orderObject
+
+    orderObject = order()
     robotState = state()
     rospy.loginfo("Otwieramy port!")
     ser = serial.Serial()
@@ -71,15 +127,19 @@ def talker():
     pub = rospy.Publisher('RaspberryControlWriter',String,queue_size=10)
     rospy.init_node('talker',anonymous=True)
     rate = rospy.Rate(20) #10Hz
-    #rospy.Subscriber("RaspberryRead",Int16,)
+    rospy.Subscriber("RaspberryRead",Int16,)
     pub.publish("JEDZIEMY!")
     rospy.loginfo("Jedziemy!")
-    rospy.Subscriber("RaspberryControlReader", String, callback)
+    rospy.Subscriber("RaspberryControlReader", Control, receiveOrders)
     ser.write(bytearray([constants.CONST_SERIAL_RPI_INITIALIZED]))
     time.sleep(1)
     bufferSerial = b""
     while not rospy.is_shutdown():
         try:
+            if orderObject.CheckChange() == True:
+                if orderObject.GenerateSerialMessage() == True:
+                    ser.write(orderObject.ReadSerialMessage())
+
             wait = ser.in_waiting
             if wait > 0:
                 bufferTemp = ser.read(wait)
@@ -97,19 +157,19 @@ def talker():
                     pub.publish("STATE MOVEMENT")
                     rospy.loginfo("MOV")
                     robotState.ChangeState(constants.CONST_STATE_MOVEMENT)
-                    #os.system('mpg321 /home/pi/Jaguar.mp3 &')
+                    os.system('mpg321 /home/pi/Jaguar.mp3 &')
                 elif function == constants.CONST_STATE_OBSTACLE:
                     pub.publish("STATE OBSTACLE")
                     rospy.loginfo("OBST")
                     robotState.ChangeState(constants.CONST_STATE_OBSTACLE)
                     if(robotState.GetLimitSwitches() > 0):
                         pass
-                        #os.system('mpg321 /home/pi/Torture.mp3 &')
+                        os.system('mpg321 /home/pi/Torture.mp3 &')
                 elif function == constants.CONST_STATE_STAIRS:
                     pub.publish("STATE STAIRS")
                     rospy.loginfo("STAIRS")
                     robotState.ChangeState(constants.CONST_STATE_STAIRS)
-                    #os.system('mpg321 /home/pi/Fall.mp3 &')
+                    os.system('mpg321 /home/pi/Fall.mp3 &')
                 elif function == constants.CONST_STATE_STOP:
                     pub.publish("STATE STOP")
                     rospy.loginfo("STOP")
